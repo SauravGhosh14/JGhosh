ed54300df136d548a78d463e491c9984457f1360
Saurav Ghosh <saurav.ghosh14@gmail.com>
2018-04-20 13:18:42 +0530

    12

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+//var $form = $('form#jghoshhealthchart'),
+//    url = 'https://script.google.com/macros/s/AKfycbzVGSMo2EFvvWaEfArX1bQBmLk4vawVNQzB_9F6yN6qet1KyAY9/exec'
+//
+//$('#submit-form').on('click', function(e) {
+//  e.preventDefault();
+//  var jqxhr = $.ajax({
+//    url: url,
+//    method: "GET",
+//    dataType: "json",
+//    data: $form.serializeObject()
+//  }).success(
+//    // do something
+//      alert("The paragraph was clicked.");
+//  );
+//})
+
+$(document).ready(function(){
 var $form = $('form#jghoshhealthchart'),
-    url = 'https://script.google.com/macros/s/AKfycbzVGSMo2EFvvWaEfArX1bQBmLk4vawVNQzB_9F6yN6qet1KyAY9/exec'
+    url = 'https://script.google.com/macros/s/AKfycbzVGSMo2EFvvWaEfArX1bQBmLk4vawVNQzB_9F6yN6qet1KyAY9/exec';
 
 $('#submit-form').on('click', function(e) {
   e.preventDefault();
@@ -13,3 +30,5 @@ $('#submit-form').on('click', function(e) {
       alert("The paragraph was clicked.");
   );
 })
+});
+
