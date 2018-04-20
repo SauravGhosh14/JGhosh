d511b9e80402972912ba609470f028b1d309b7f4
Saurav Ghosh <saurav.ghosh14@gmail.com>
2018-04-20 12:18:19 +0530

    05

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var $form = $('form#jghoshhealthchart'),
+    url = 'https://script.google.com/macros/s/AKfycbzVGSMo2EFvvWaEfArX1bQBmLk4vawVNQzB_9F6yN6qet1KyAY9/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
